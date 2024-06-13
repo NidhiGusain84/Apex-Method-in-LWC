5e394a834880d87cca5c35e20e6162d29ea1fa52
NidhiGusain84 <nidhi.nidhi85@gmail.com>
2024-06-13 13:08:28 -0400

    Wired Apex Method to a function

--- force-app/main/default/lwc/searchAccount/searchAccount.js
+++ force-app/main/default/lwc/searchAccount/searchAccount.js
@@ -3,10 +3,21 @@ import findAccount from '@salesforce/apex/AccountController.findAccounts';
 
 export default class SearchAccount extends LightningElement {
     searchKey;
+    accounts;
+    error;
 
     @wire(findAccount, {
         searchString: '$searchKey'
-    })accounts;
+    })wiredAccounts({data, error}){
+        if(data){
+            this.accounts = data;
+            this.error = undefined;
+        }else{
+            this.accounts = undefined;
+            this.error = error;
+        }
+       
+    };
 
     handleChange(event){
         this.searchKey = event.target.value;
